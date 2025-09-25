be8eef88951bf56daea95c587402cd81e271dd86
jcarlossc <josecscosta@fac.pe.senac.br>
2025-09-25 17:05:57 -0300

    Arquivo para criação da tabela usuarios.

---
+++ source-sqlite/db_schema.R
@@ -0,0 +1,14 @@
+# Arquivo responsável por criar uma tabela no SQLite.
+
+# Criar tabela de exemplo (Usuários).
+create_tables <- function(con) {
+  query <- "
+  CREATE TABLE IF NOT EXISTS usuarios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    idade INTEGER
+  );
+  "
+  dbExecute(con, query)
+}
